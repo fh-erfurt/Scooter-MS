5ea038d573e32ab9b3be95ad4f1adb6891aa328e
Retch <retch100@gmail.com>
2020-12-07 10:35:21 +0100

    updated AreaTest.java

--- src/test/java/de/teamshrug/scooterms/AreaTest.java
+++ src/test/java/de/teamshrug/scooterms/AreaTest.java
@@ -1,15 +1,27 @@
 package de.teamshrug.scooterms;
 
 import de.teamshrug.scooterms.Area;
+import junit.framework.Assert;
 import org.junit.jupiter.api.Test;
+//import junit.framework.Test;
 
 
 public class AreaTest {
 
-    @Test
+    private final boolean isinarea = true;
 
-    public void firstTest()
+
+
+    @Test
+    public void isInAreaTest()
     {
+
+
+        Area TestArea = new Area("TestArea",50.94f,51.11f, 10.98f,11.04f);
+        Coordinate TestPositionInArea = new Coordinate(50.978966f,11.029587f);
+
+
+        Assert.assertEquals(isinarea, TestArea.isInArea(TestPositionInArea));
 
     }
 
