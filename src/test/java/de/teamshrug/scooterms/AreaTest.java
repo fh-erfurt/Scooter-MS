47bcca35382ff83fc4d8e8eef5e84b251703f11e
Retch <retch100@gmail.com>
2020-12-07 10:20:05 +0100

    updated AreaTest.java

--- src/test/java/de/teamshrug/scooterms/AreaTest.java
+++ src/test/java/de/teamshrug/scooterms/AreaTest.java
@@ -1,10 +1,16 @@
 package de.teamshrug.scooterms;
 
 import de.teamshrug.scooterms.Area;
+import org.junit.jupiter.api.Test;
 
 
 public class AreaTest {
 
+    @Test
 
+    public void firstTest()
+    {
+
+    }
 
 }
