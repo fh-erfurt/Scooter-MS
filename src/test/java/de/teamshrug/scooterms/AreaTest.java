d05e7533bbf794d3e2554b63aa84a82f48740b1e
Retch <retch100@gmail.com>
2020-12-08 11:05:33 +0100

    updated AreaTest.java

--- src/test/java/de/teamshrug/scooterms/AreaTest.java
+++ src/test/java/de/teamshrug/scooterms/AreaTest.java
@@ -1,7 +1,7 @@
 package de.teamshrug.scooterms;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 
