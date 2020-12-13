a0147b47c9ab8ba1a879b803eb00932ce0412762
Retch <retch100@gmail.com>
2020-12-13 12:55:59 +0100

    updated AreaTest

--- src/test/java/de/teamshrug/scooterms/AreaTest.java
+++ src/test/java/de/teamshrug/scooterms/AreaTest.java
@@ -5,14 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 
-
 public class AreaTest {
-
-    private final boolean isinarea = true;
-
-
-
-
 
     @Test
     public void isInAreaTest()
@@ -20,7 +13,7 @@ public class AreaTest {
         Area TestArea = new Area("TestArea",50.94f,51.11f, 10.98f,11.04f);
         Coordinate TestPositionInArea = new Coordinate(50.978966f,11.029587f);
 
-        assertEquals(isinarea, TestArea.isInArea(TestPositionInArea));
+        assertTrue(TestArea.isInArea(TestPositionInArea), "If ndegree is between ndegree1 and ndegree2 (same for edegree), the scooter is in the area");
     }
 
 }
