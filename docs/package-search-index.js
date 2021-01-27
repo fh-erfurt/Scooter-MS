8f6f7b7bfc0f673ef65da3c24c6023e6fd3951dd
JavaDoc Generator <me@email.org>
2021-01-27 15:57:43 +0000

    Auto-Generated JavaDoc

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.teamshrug.scooterms"},{"l":"de.teamshrug.scooterms.exceptions"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"de.teamshrug.scooterms"},{"l":"de.teamshrug.scooterms.enums"},{"l":"de.teamshrug.scooterms.exceptions"}];updateSearchResults();
