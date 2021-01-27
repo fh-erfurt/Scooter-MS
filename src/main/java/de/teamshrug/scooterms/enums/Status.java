eed30be5b4aa5ac5a94b164b37d918691f22421e
giraffe187 <jonas.waldhelm@fh-erfurt.de>
2021-01-27 17:17:31 +0100

    added javadoc

--- src/main/java/de/teamshrug/scooterms/enums/Status.java
+++ src/main/java/de/teamshrug/scooterms/enums/Status.java
@@ -1,8 +1,36 @@
 package de.teamshrug.scooterms.enums;
 /**
- * The enum Status consists of these types: ready, lowonbattery, inuse, charging, damaged, maintenance. Every Scooter can change his 'Status' to a different one.
+ * The enum Status consists of the types: ready, lowonbattery, inuse, charging, damaged, maintenance. Every Scooter can change his 'Status' to a different one.
  */
 public enum Status
 {
-    ready,lowonbattery,inuse,charging,damaged,maintenance
+    /**
+     * The scooter is ready to use
+     */
+    ready,
+
+    /**
+     * The scooter gets the state, if the battery is lower or equals 20
+     */
+    lowonbattery,
+
+    /**
+     * The scooter is currently in use
+     */
+    inuse,
+
+    /**
+     * The scooter is charging
+     */
+    charging,
+
+    /**
+     * The scooter was reported as damaged
+     */
+    damaged,
+
+    /**
+     * The scooter is being serviced
+     */
+    maintenance
 }
